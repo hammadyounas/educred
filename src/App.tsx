6121fe8ccaad5870e5c627f898352bff8549c808
Hammad Younas <m.hammadyounas@outlook.com>
2024-06-21 14:35:33 +0500

    minor

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,15 @@ const App: React.FC = () => {
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            My React App
+            Educred
           </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
       <Container>
-        <Typography variant="h4" component="h1" gutterBottom>
+        {/* <Typography variant="h4" component="h1" gutterBottom>
           Welcome to My React App
-        </Typography>
+        </Typography> */}
       </Container>
     </div>
   );

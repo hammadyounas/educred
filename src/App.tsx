1a83b5fd8a434dea31ebb1c9dea5fa4ec39bce95
Hammad Younus <ifrashafique123@gmail.com>
2024-06-21 16:34:44 +0500

    start landing page design

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography, Button, Container } from '@mui/material';
+import Home from './pages/LandingPage/Home';
 
 const App: React.FC = () => {
   return (
     <div>
-      <AppBar position="static">
+      <Home />
+      {/* <AppBar position="static">
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Educred
           </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
-      </AppBar>
+      </AppBar> */}
       <Container>
         {/* <Typography variant="h4" component="h1" gutterBottom>
           Welcome to My React App
